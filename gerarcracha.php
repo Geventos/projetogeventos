4c38455ddd951dfab52bf48cfb90fa7732e467eb
Hélio Evangelista de Medeiros Júnior <38916577+helioemjunior@users.noreply.github.com>
2018-10-01 15:07:16 -0300

    Primeiro commit

---
+++ gerarcracha.php
@@ -0,0 +1,95 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<head>
+	<title>SisGEv | Gerar Crachá</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="keywords" content="Colored Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
+	Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+	<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+	<?php
+	include("links.php");
+	include("scripts.php");
+	?>
+</head>
+<body class="dashboard-page">
+	<!--MENU e TOPO-->
+	<?php
+	include("menu.php");
+	include("topo.php");
+	?>
+	<!--FIM MENU e TOPO-->
+	<div class="main-grid">
+		<div class="agile-grids">
+			<!-- grids -->
+			<div class="grids">
+				<div class="progressbar-heading grids-heading">
+					<h2 align="center">Gerar Crachá</h2>
+				</div>
+				<div class="panel panel-widget forms-panel">
+					<div class="form-title">
+						<div class="row">
+							<div class="col-sm-12">
+								<div class="col-md-10">
+									<p><a href="#"><i class="fa fa-plus-square"></i></i></a> Gerar Crachá</p>
+								</div>
+							
+							</div>
+						</div>
+					</div>
+					<div class="bs-docs-example wow fadeInUp animated" data-wow-delay=".5s">
+						<table class="table table-hover">
+							<thead>
+								<tr>
+									<th>Nome</th>
+									<th>Evento</th>
+									<th>Gerar</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td align="center">Jane Doe</td>
+									<td align="center">SIC - Semana de Informática</td>
+									<td align="center"><a href="#" title="Gerar Crachá"><i  class="fa fa-plus-square"></i></a></td>
+								</tr>
+								<tr>
+									<td align="center">John Doe</td>
+									<td align="center">SIC - Semana de Informática</td>
+									<td align="center"><a href="#" title="Gerar Crachá"><i  class="fa fa-plus-square"></i></a></td>
+								</tr>
+							</tbody>
+						</table>
+						<hr>
+						<nav align="center">
+							<ul class="pagination pagination-lg">
+								<li><a href="#" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
+								<li><a href="#">1</a></li>
+								<li><a href="#">2</a></li>
+								<li><a href="#">3</a></li>
+								<li><a href="#">4</a></li>
+								<li><a href="#">5</a></li>
+								<li><a href="#" aria-label="Next"><span aria-hidden="true">»</span></a></li>
+							</ul>
+						</nav>
+						</hr>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- //grids -->
+</div>
+</div>
+<!-- Rodapé -->
+<?php
+include("rodape.php");
+?>
+<!-- Fim Rodapé -->
+</section>
+</body>
+</html>
