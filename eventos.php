b1e75e3f89f1c4d4ce9a589ce09bbb4583dfa9c1
cristovaolima <37450799+cristovaolima@users.noreply.github.com>
2018-10-05 23:09:12 -0300

    Primeiro commit

---
+++ eventos.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html >
+  <head>
+    <title>SisGEv | Eventos</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="generator" content="Mobirise v4.8.1, mobirise.com">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+    <meta name="description" content="Web Page Generator Description">
+    <?php require_once("linksfe.php"); ?>
+  </head>
+  <body>
+    <?php require_once("menufe.php"); ?>
+    <section class="section-table cid-r5loR0dIRC" id="table1-f">
+      <div class="container container-table">
+        <h2 class="mbr-section-title mbr-fonts-style align-center pb-3 display-2">Eventos</h2>
+        <div class="table-wrapper">
+          <div class="container">
+            <div class="row search">              
+                <div class="dataTables_filter">
+                  <label class="searchInfo mbr-fonts-style">Buscar:</label>
+                  <input class="form-control input-sm">
+                </div> 
+            </div>
+          </div>
+          <div class="container scroll">
+            <table class="table isSearch" cellspacing="0">
+              <thead>
+                <tr>
+                  <th>Nome</th>
+                  <th>Data Inicio</th>
+                  <th>Data Fim</th>
+                  <th title="Detalhes do evento"></th>
+                </tr>
+              </thead>
+              <tbody>          
+                <tr>             
+                  <td class="body-item mbr-fonts-style display-7">XVIII EPOPET - ENCONTRO POTIGUAR DOS GRUPOS PET</td>
+                  <td class="body-item mbr-fonts-style display-7">19/09/2018</td>
+                  <td class="body-item mbr-fonts-style display-7">19/09/2018</td>
+                  <td><a href=""><i class="mbri-pages" title="Detalhes do evento"></i></a></i></td>
+                </tr>
+                <tr>            
+                  <td class="body-item mbr-fonts-style display-7">I SEMINÁRIO DE DIREITO DIGITAL - NEDDIG </td>
+                  <td class="body-item mbr-fonts-style display-7">20/09/2018</td>
+                  <td class="body-item mbr-fonts-style display-7">20/09/2018</td>
+                  <td><a href=""><i class="mbri-pages" title="Detalhes do evento"></i></a></td>
+                </tr>           
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </section>
+    <?php require_once("rodapefe.php"); ?>
+    <script src="assets/web/assets/jquery/jquery.min.js"></script>
+    <script src="assets/popper/popper.min.js"></script>
+    <script src="assets/tether/tether.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/datatables/jquery.data-tables.min.js"></script>
+    <script src="assets/datatables/data-tables.bootstrap4.min.js"></script>
+    <script src="assets/dropdown/js/script.min.js"></script>
+    <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+    <script src="assets/smoothscroll/smooth-scroll.js"></script>
+    <script src="assets/theme/js/script.js"></script>
+  </body>
+</html>
