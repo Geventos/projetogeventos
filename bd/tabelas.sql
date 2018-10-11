ea6e173324540a3b79d427c9079fd93409398d40
cristovaolima <37450799+cristovaolima@users.noreply.github.com>
2018-10-10 21:08:20 -0300

    Correçao primary key

--- bd/tabelas.sql
+++ bd/tabelas.sql
@@ -8,31 +8,33 @@ CREATE TABLE `usuario` (
   `senha` varchar(20) not null,
   `email` varchar(25) not null,
   `permissao` int(10) not null,
-  PRIMARY KEY (`id_usuario`)
+  primary key (`id_usuario`)
 ) ;
 
 CREATE TABLE `permissao`(
-`id_evento` int(10)  not null,
-`id_usuario` int(10) not null,
-`tipo_permissao` int(10) not null
+  `id_evento` int(10)  not null,
+  `id_usuario` int(10) not null,
+  `tipo_permissao` int(10) not null
 );
 
 CREATE TABLE `evento`(
-id_evento int(10) auto_increment,
-nome varchar(50) not null,
-data_ini date not null,
-data_fim date not null,
-sobre text,
-programacao text,
-contatos varchar(100)
+  `id_evento` int(10) auto_increment,
+  `nome` varchar(50) not null,
+  `data_ini` date not null,
+  `data_fim` date not null,
+  `sobre` text,
+  `programacao` text,
+  `contatos` varchar(100),
+  primary key (`id_evento`)
 );
 
 create table `inscrito`(
-id_participante int(10) auto_increment,
-nome_participante varchar(50) not null,
-cpf int(11) not null,
-instituicao varchar(50),
-id_evento int(10) not null
+   `id_participante` int(10) auto_increment,
+   `nome_participante` varchar(50) not null,
+   `cpf` int(11) not null,
+   `instituicao` varchar(50),
+   `id_evento` int(10) not null,
+	primary key (`id_participante`)
 );
 
 
