5598529cff84a0133ebb424e5a1afd3aab429b2c
Hélio Evangelista de Medeiros Júnior <38916577+helioemjunior@users.noreply.github.com>
2018-10-13 14:12:54 -0300

    Novas consultas

--- bd/consultas.sql
+++ bd/consultas.sql
@@ -9,3 +9,11 @@ select * from inscrito where nome_participante like '%t%';
 
 #Permissão
 select * from permissao;
+
+#Atualizar 
+#Puxar o evento
+select * from evento where id_evento = "paramentro" limit 1;
+
+#Atualizar no banco
+update evento set nome = '$nome', data_ini = '$data_ini', data_fim = '$data_fim', sobre = '$sobre', programacao = '$programacao', contatos = '$contatos'
+WHERE id_evento = '$id_evento'
