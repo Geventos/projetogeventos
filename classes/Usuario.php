4a78b3f40a3b83db3781e17972fc0463f7faa222
cristovaolima <37450799+cristovaolima@users.noreply.github.com>
2018-10-20 10:02:09 -0300

    Primeiro commit

---
+++ classes/Usuario.php
@@ -0,0 +1,31 @@
+<?php
+class Usuario {
+    private $nomecompleto;
+    private $email;
+    private $senha;
+    
+    function Usuario($nomecompleto, $email, $senha) {
+        $this->nomecompleto = $nomecompleto;
+        $this->email = $email;
+        $this->senha = $senha;
+    }
+    
+    function getNomecompleto() {
+        return $this->nomecompleto;
+    }
+    function getEmail() {
+        return $this->email;
+    }
+    function getSenha() {
+        return $this->senha;
+    }
+    function setNomecompleto($nomecompleto) {
+        $this->nomecompleto = $nomecompleto;
+    }
+    function setEmail($email) {
+        $this->email = $email;
+    }
+    function setSenha($senha) {
+        $this->senha = $senha;
+    }   
+}
