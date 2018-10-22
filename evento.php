edefafed54b9b8f97ec63c0b769a8497d5963146
Hélio Evangelista de Medeiros Júnior <38916577+helioemjunior@users.noreply.github.com>
2018-10-22 15:59:20 -0300

    Lógica de listar na tabela

--- evento.php
+++ evento.php
@@ -1,7 +1,17 @@
+<?php  
+require_once("classes/Conexao.php");
+require_once("classes/DALEvento.php");
+$cx = new Conexao();
+$codigo  = $_GET['id_evento'];
+$consulta = "SELECT * FROM evento WHERE id_evento = $codigo limit 1";
+$con = mysqli_query($cx->getBanco(), $consulta);
+$linha = mysqli_fetch_assoc($con);
+?>
+
 <!DOCTYPE html>
 <html >
     <head>
-        <title>SisGEv | Evento - [Evento] </title>
+        <title>SisGEv | Evento - <?php echo $linha['nome']?> </title>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="generator" content="Mobirise v4.8.1, mobirise.com">
@@ -16,8 +26,8 @@
                 <div class="col-md-12 multi-horizontal">
                     <div class="row row-sm-offset">
                         <div class="col-md-9 multi-horizontal">
-                            <h2 class="mbr-section-title align-center pb-3 mbr-fonts-style display-2">[NOME EVENTO]</h2>
-                            <h3 class="mbr-section-subtitle align-center mbr-light pb-3 mbr-fonts-style display-5">[DATA]</h3>
+                            <h2 class="mbr-section-title align-center pb-3 mbr-fonts-style display-2"><?php echo $linha['nome']?></h2>
+                            <h3 class="mbr-section-subtitle align-center mbr-light pb-3 mbr-fonts-style display-5"><?php echo date ("d/m/Y", strtotime($linha['data_ini']));?> a <?php echo date ("d/m/Y", strtotime($linha['data_fim']));?> </h3>
                         </div>
                         <div class="col-md-3 multi-horizontal">
                             <div class="row row-sm-offset">
@@ -37,13 +47,13 @@
                             <div class="col-md-8 multi-horizontal" data-for="name">
                                 <div class="form-group">
                                     <label class="form-control-label mbr-fonts-style display-7">Sobre o evento</label>
-                                    <textarea type="text" class="form-control" rows="7"></textarea>
+                                    <textarea type="text" class="form-control" rows="7" disabled> <?php echo $linha['sobre'];?></textarea>
                                 </div>
                             </div>
                             <div class="col-md-4 multi-horizontal" data-for="name">
                                 <div class="form-group">
                                     <label class="form-control-label mbr-fonts-style display-7">Contatos</label>
-                                    <textarea type="text" class="form-control" rows="7"></textarea>
+                                    <textarea type="text" class="form-control" rows="7" disabled><?php echo $linha['contatos'];?></textarea>
                                 </div>
                             </div>
                         </div>
@@ -51,7 +61,7 @@
                             <div class="col-md-12 multi-horizontal">
                                 <div class="form-group">
                                     <label class="form-control-label mbr-fonts-style display-7">Programaçao</label>
-                                    <textarea type="text" class="form-control" rows="7"></textarea>
+                                    <textarea type="text" class="form-control" rows="7" disabled><?php echo $linha['programacao'];?></textarea>
                                 </div>
                             </div>
                         </div>
