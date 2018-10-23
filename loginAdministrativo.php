7abece8b7cc8ff18e5a2932d9cb92600660ad982
Hélio Evangelista de Medeiros Júnior <38916577+helioemjunior@users.noreply.github.com>
2018-10-23 16:05:56 -0300

    Primeiro commit

---
+++ loginAdministrativo.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html >
+  <head>
+    <!-- Site made with Mobirise Website Builder v4.8.1, https://mobirise.com -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="generator" content="Mobirise v4.8.1, mobirise.com">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+    <link rel="shortcut icon" href="assets/images/1jojl1fomkx9wypfbe43d6kivkbrbzomr...ewxs1m3emoajtlsirj.pdf-122x73.png" type="image/x-icon">
+    <meta name="description" content="Website Builder Description">
+    <title>Login</title>
+    <link rel="stylesheet" href="assets/web/assets/mobirise-icons-bold/mobirise-icons-bold.css">
+    <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+    <link rel="stylesheet" href="assets/tether/tether.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="assets/dropdown/css/style.css">
+    <link rel="stylesheet" href="assets/animatecss/animate.min.css">
+    <link rel="stylesheet" href="assets/theme/css/style.css">
+    <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+    
+    
+    
+  </head>
+  <body>
+    <?php require_once("menufe.php");?>
+    <section class="engine"><a href="https://mobiri.se/c">website builder</a></section><section class="cid-qVb72QGGYL mbr-fullscreen mbr-parallax-background" id="header15-17">
+    
+    <div class="mbr-overlay" style="opacity: 0.5; background-color: black;"></div>
+    <div class="container align-right">
+      <div class="row">
+        <div class="mbr-white col-lg-8 col-md-7 content-container">
+          <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-1" align="center">LOGIN</h1>
+        </div>
+        <div class="col-lg-4 col-md-5">
+          <div class="form-container">
+            <div class="media-container-column" data-form-type="formoid">
+              <div data-form-alert="" hidden="" class="align-center">Thanks for filling out the form!</div>
+              <form class="mbr-form" action="https://mobirise.com/" method="post" data-form-title="Mobirise Form"><input type="hidden" name="email" data-form-email="true" value="m8eRgOzHz+D5iZDqa2WKhwMGPzYXkddH/YJ6eH8TS7YpNrKzuvLCkm2bIt5nlgCb11Ai3EHXyBtCy/diK2hIPnvRzY6S8EFXcgL7jzokEsHGdXVNd8s8iPmK3T4Vbd0s" data-form-field="Email">
+              <div data-for="name">
+                <div class="form-group">
+                  <input type="text" class="form-control px-3" name="name" data-form-field="Name" placeholder="User" required="" id="name-header15-17">
+                </div>
+              </div>
+              
+              <div data-for="phone">
+                <div class="form-group">
+                  <input type="tel" class="form-control px-3" name="phone" data-form-field="Phone" placeholder="Passoword" id="phone-header15-17">
+                </div>
+              </div>
+              <div class="row">
+                
+                  <div class=col-lg-6">
+                    <P align="right">PROBLEMAS COM LOGIN?</P>
+                  </div>
+                  <div class=col-lg-6">
+                    <span class="input-group-btn"><button href="" type="submit" class="btn btn-form btn-danger display-4">CADASTRAR</button></span>
+                  </div>
+                  <div class=col-lg-6">
+                    <span class="input-group-btn"><button href="" type="submit" class="btn btn-form btn-danger display-4">ENTRAR</button></span>
+                  </div>
+                </div>
+              
+              
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+</section>
+<?php require_once("rodapefe.php"); ?>
+<script src="assets/web/assets/jquery/jquery.min.js"></script>
+<script src="assets/popper/popper.min.js"></script>
+<script src="assets/tether/tether.min.js"></script>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/dropdown/js/script.min.js"></script>
+<script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+<script src="assets/viewportchecker/jquery.viewportchecker.js"></script>
+<script src="assets/parallax/jarallax.min.js"></script>
+<script src="assets/smoothscroll/smooth-scroll.js"></script>
+<script src="assets/theme/js/script.js"></script>
+<script src="assets/formoid/formoid.min.js"></script>
+
+
+
+<input name="animation" type="hidden">
+</body>
+</html>
