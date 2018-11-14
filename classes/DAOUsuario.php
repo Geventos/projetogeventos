c18b2d2f576f467c5aefa3360ebdb6e03e95ba15
cristovaolima <37450799+cristovaolima@users.noreply.github.com>
2018-11-13 21:48:30 -0300

    Atualização no nome do arquivo

---
+++ classes/DAOUsuario.php
@@ -0,0 +1,45 @@
+<?php
+require_once("classes/Conexao.php");
+require_once("classes/Usuario.php");
+class DAOUsuario {
+    private $conexao;
+    
+    function __construct($conexao) {
+        $this->conexao = $conexao;
+    }
+    
+    public function Criar($usuario) {
+        $sql = "insert into usuario(nome_completo, senha, email) values('";
+        $sql = $sql . $usuario->getNomecompleto() . "','";
+        $sql = $sql . $usuario->getSenha() . "','";
+        $sql = $sql . $usuario->getEmail() . "')";
+        $banco = $this->conexao->getBanco();
+        $banco->query($sql);
+        $linhas = mysqli_affected_rows($banco);
+        $this->conexao->Desconectar();        
+        if($linhas == 1){
+            echo "<script type='text/javascript'>
+                    alert('Cadastro efetuado com sucesso!');
+                </script>";
+         }else{
+            echo "<script type='text/javascript'>
+                    alert('Cadastro NÃO efetuado!');
+                </script>";
+        }
+    }
+    
+    function login($email, $senha){
+        $sql = "select * from usuario where email= '";
+        $sql = $sql . $email . "' and senha='";
+        $sql = $sql . $senha . "'";
+        $banco = $this->conexao->getBanco();
+        $banco->query($sql);
+        $linhas = mysqli_affected_rows($banco);
+        $this->conexao->Desconectar();        
+        if($linhas > 1){
+            return true;
+         }else{
+             return FALSE;
+        }
+    }
+}
