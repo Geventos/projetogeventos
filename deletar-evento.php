f99129acd7c7e7dbb1053086f57448128c8b0392
cristovaolima <37450799+cristovaolima@users.noreply.github.com>
2018-11-15 16:36:08 -0300

    Logica de sessão

--- deletar-evento.php
+++ deletar-evento.php
@@ -1,8 +1,14 @@
 <?php
-    require_once("classes/Conexao.php");
-    require_once("classes/DAOEvento.php");
-    $idevento = $_GET['id_evento'];
-    $cx = new Conexao();
-    $dao = new DAOEvento($cx);
-    $dao->Deletar($idevento);        
+	session_start();
+	if(!empty($_SESSION['id_usuario'])){
+	    require_once("classes/Conexao.php");
+	    require_once("classes/DAOEvento.php");
+	    $idevento = $_GET['id_evento'];
+	    $cx = new Conexao();
+	    $dao = new DAOEvento($cx);
+	    $dao->Deletar($idevento);
+	}else{
+		header("Location: login.php");
+	}
+	include_once("sessao.php");    
 ?>
