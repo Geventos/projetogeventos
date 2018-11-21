f380be221a2c241b28beb2437738b7e8848d9135
cristovaolima <37450799+cristovaolima@users.noreply.github.com>
2018-11-21 10:19:47 -0300

    Correção para fechar conexão

--- classes/DAOEvento.php
+++ classes/DAOEvento.php
@@ -18,8 +18,7 @@ class DAOEvento {
         $sql = $sql . $evento->getContatos() . "')";
         $banco = $this->conexao->getBanco();
         $banco->query($sql);
-        $linhas = mysqli_affected_rows($banco);
-        $this->conexao->Desconectar();        
+        $linhas = mysqli_affected_rows($banco);        
         if($linhas == 1){
             echo "<script type='text/javascript'>
                     alert('Cadastro efetuado com sucesso!');
@@ -31,6 +30,7 @@ class DAOEvento {
                     location.href='adm-eventos.php';
                 </script>";
         }
+        mysqli_close($this->conexao);
     }
 
     public function Deletar($idevento) {
@@ -38,8 +38,7 @@ class DAOEvento {
         $sql = $sql . $idevento . ";";
         $banco = $this->conexao->getBanco();
         $banco->query($sql);
-        $linhas = mysqli_affected_rows($banco);
-        $this->conexao->Desconectar();        
+        $linhas = mysqli_affected_rows($banco);        
         if($linhas == 1){
             echo "<script type='text/javascript'>
                     alert('Remoção efetuada com sucesso!');
@@ -51,6 +50,7 @@ class DAOEvento {
                     location.href='adm-eventos.php';
                 </script>";
         }
+        mysqli_close($this->conexao);
     }
 
     public function ValidarPresenca($idevento, $idinscrito) {
@@ -62,8 +62,7 @@ class DAOEvento {
         $sql = $sql . $idinscrito . " and id_evento = " . $idevento . ";";
         $banco = $this->conexao->getBanco();
         $banco->query($sql);
-        $linhas = mysqli_affected_rows($banco);
-        $this->conexao->Desconectar();        
+        $linhas = mysqli_affected_rows($banco);        
         if($linhas > 0){
             echo "<script type='text/javascript'>
                     alert('Presença efetuada com sucesso!');
@@ -75,14 +74,14 @@ class DAOEvento {
                     location.href='validarpresenca.php?id_evento=$idevento';
                 </script>";
         }
+        mysqli_close($this->conexao);
     }
 
     public function Atualizar($evento, $id_evento) {
         $sql = "update evento set nome ='" . $evento->getNome() . "', data_ini='" .  $evento->getDatainicio() . "', data_fim='" . $evento->getDatafim() . "', sobre= '" . $evento->getSobre() . "', programacao='" . $evento->getProgramacao() . "', contatos='" . $evento->getContatos() . "' where id_evento='" . $id_evento . "';";
         $banco = $this->conexao->getBanco();
         $banco->query($sql);
-        $linhas = mysqli_affected_rows($banco);
-        $this->conexao->Desconectar();        
+        $linhas = mysqli_affected_rows($banco);        
         if($linhas == 1){
             echo "<script type='text/javascript'>
                     alert('Atualização efetuada com sucesso!');
@@ -94,12 +93,14 @@ class DAOEvento {
                     location.href='eventodetalhes.php?id_evento=$id_evento';
                 </script>";
         }
+        mysqli_close($this->conexao);
     }
 
     public function Listar(){
         $consulta ="SELECT * FROM evento ORDER BY data_ini";
         $con = mysqli_query($this->conexao->getBanco(), $consulta);
         return $con;
+        mysqli_close($this->conexao);
     }
 
     public function Exibir($idevento){
@@ -107,6 +108,7 @@ class DAOEvento {
         $con = mysqli_query($this->conexao->getBanco(), $consulta);
         $linha = mysqli_fetch_assoc($con);
         return $linha;
+        mysqli_close($this->conexao);
     }
 
 }
