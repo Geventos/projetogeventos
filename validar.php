1b5b74b1b5029123dc3a7f533993ccc2bf70136f
cristovaolima <37450799+cristovaolima@users.noreply.github.com>
2018-12-02 20:54:42 -0300

    Correção de instancia

--- validar.php
+++ validar.php
@@ -1,9 +1,9 @@
 <?php 
-	require_once("classes/Conexao.php");
+	require_once("classes/Conn.php");
 	require_once("classes/DAOEvento.php");
 	$idevento = $_GET['id_evento'];
 	$idi = $_GET['id_inscrito'];
-	$cx = new Conexao();
+	$cx = Conn::getInstance();
 	$dao = new DAOEvento($cx);
 	$dao->ValidarPresenca($idevento, $idi);
 ?>
