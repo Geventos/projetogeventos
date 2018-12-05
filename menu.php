e3c89bf789757b42804be5cba509c665e460819e
Hélio Evangelista de Medeiros Júnior <38916577+helioemjunior@users.noreply.github.com>
2018-12-04 21:03:47 -0300

    Atualização

--- menu.php
+++ menu.php
@@ -1,39 +1,3 @@
 <nav class="main-menu">
-	<ul>
-		<li>
-			<a href="homebe.php">
-				<i class="fa fa-home nav_icon" aria-hidden="true"></i>
-				<span class="nav-text">
-					Home
-				</span>
-			</a>
-		</li>
-		<li>
-
 	
-
-			<a href="adm-eventos.php">
-				<i class="fa fa-calendar nav_icon" aria-hidden="true"></i>
-				<span class="nav-text">
-					Eventos
-				</span>
-			</a>
-		</li>
-		<li>
-			<a href="permissoes.php" align="center" >
-				<i  class=""></i>
-				<span class="nav-text">
-					Permissões
-				</span>
-			</a>
-		</li>
-		<li>
-			<a href="gerarCracha.php" align="center">
-				<i  class=""></i>
-				<span class="nav-text">
-					Gerar Crachá
-				</span>
-			</a>
-		</li>		
-	</ul>
 </nav>
