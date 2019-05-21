3c16110da770af5d26f40a1f18a77235c813902a
cristovaolima <37450799+cristovaolima@users.noreply.github.com>
2019-05-21 10:41:25 -0300

    Correção nos metodos de criar, atualizar e listar evento

--- classes/DAOEvento.php
+++ classes/DAOEvento.php
@@ -8,31 +8,34 @@ class DAOEvento {
         $this->conexao = Conexao::getInstance();
     }
     
-    public function Criar($evento) {
-        $sql = "insert into evento(nome, data_ini, data_fim, sobre, programacao, contatos) values('";
+    public function criar($evento) {
+        $sql = "insert into evento(nome, data_ini, data_fim, sobre, programacao, contatos, banner, logo, url) values('";
         $sql = $sql . $evento->getNome() . "','";
         $sql = $sql . $evento->getDatainicio() . "','";
         $sql = $sql . $evento->getDatafim() . "','";
         $sql = $sql . $evento->getSobre() . "','";
         $sql = $sql . $evento->getProgramacao() . "','";
-        $sql = $sql . $evento->getContatos() . "')";
+        $sql = $sql . $evento->getContatos() . "','";
+        $sql = $sql . $evento->getBanner() . "','";
+        $sql = $sql . $evento->getLogo() . "','";
+        $sql = $sql . $evento->getUrlpersonalizada() . "');";
         $banco = $this->conexao->getBanco();
         $banco->query($sql);
         $linhas = mysqli_affected_rows($banco);        
         if($linhas == 1){
             echo "<script type='text/javascript'>
                     alert('Cadastro efetuado com sucesso!');
-                    location.href='adm-eventos.php';
+                    location.href='adm-eventos';
                 </script>";
          }else{
             echo "<script type='text/javascript'>
                     alert('Cadastro NÃO efetuado!');
-                    location.href='adm-eventos.php';
+                    location.href='adm-eventos';
                 </script>";
         }
     }
 
-    public function Deletar($idevento) {
+    public function deletar($idevento) {
         $sql = "delete from evento where id_evento = ";
         $sql = $sql . $idevento . ";";
         $banco = $this->conexao->getBanco();
@@ -51,7 +54,7 @@ class DAOEvento {
         }
     }
 
-    public function ValidarPresenca($idevento, $idinscrito) {
+    public function validarPresenca($idevento, $idinscrito) {
         $consultadia = "select NOW()";
         $con = mysqli_query($this->conexao->getBanco(), $consultadia);
         $linha = mysqli_fetch_array($con);
@@ -74,8 +77,16 @@ class DAOEvento {
         }
     }
 
-    public function Atualizar($evento, $id_evento) {
-        $sql = "update evento set nome ='" . $evento->getNome() . "', data_ini='" .  $evento->getDatainicio() . "', data_fim='" . $evento->getDatafim() . "', sobre= '" . $evento->getSobre() . "', programacao='" . $evento->getProgramacao() . "', contatos='" . $evento->getContatos() . "' where id_evento='" . $id_evento . "';";
+    public function atualizar($evento, $id_evento) {
+        $sql = "update evento set nome ='" . $evento->getNome() . "', data_ini='";
+        $sql = $sql . $evento->getDatainicio() . "', data_fim='";
+        $sql = $sql . $evento->getDatafim() . "', sobre= '";
+        $sql = $sql . $evento->getSobre() . "', programacao='";
+        $sql = $sql . $evento->getProgramacao() . "', contatos='";
+        $sql = $sql . $evento->getContatos() . "', banner='";
+        $sql = $sql . $evento->getBanner() . "', logo='";
+        $sql = $sql . $evento->getLogo() . "', url='";
+        $sql = $sql . $evento->getUrlpersonalizada() . "' where id_evento='" . $id_evento . "';";
         $banco = $this->conexao->getBanco();
         $banco->query($sql);
         $linhas = mysqli_affected_rows($banco);        
@@ -92,20 +103,20 @@ class DAOEvento {
         }
     }
 
-    public function Listar(){
+    public function listar(){
         $consulta ="SELECT * FROM evento ORDER BY data_ini";
-        $con = mysqli_query($this->conexao->getBanco(), $consulta);
-        return $con;
+        $retorno = mysqli_query($this->conexao->getBanco(), $consulta);
+        return $retorno;
     }
 
-    public function Exibir($idevento){
+    public function exibir($idevento){
         $consulta = "SELECT * FROM evento WHERE id_evento = $idevento limit 1";
         $con = mysqli_query($this->conexao->getBanco(), $consulta);
         $linha = mysqli_fetch_assoc($con);
         return $linha;
     }
 
-    public function Validado($idevento){
+    public function validado($idevento){
         $consulta = "select * from inscricao where status = 'presente' and id_evento = $idevento order by time(datahora) desc limit 10;";
         $con = mysqli_query($this->conexao->getBanco(), $consulta);
         return $con;
