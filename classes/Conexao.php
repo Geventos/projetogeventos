df23a5262a483c1d3e2d38520dd04263c511bbe2
cristovaolima <37450799+cristovaolima@users.noreply.github.com>
2019-05-21 10:11:21 -0300

    Create Conexao.php

---
+++ classes/Conexao.php
@@ -0,0 +1,39 @@
+<?php
+class Conexao{	
+    // Instância da classe
+    private static $instance = null;
+    private $banco;
+
+    // Construtor privado: só a própria classe pode invocá-lo
+    private function __construct()
+    {
+        $host = "localhost";
+        $user = "root";
+        $pswd = "";
+        $db = "sisgev";
+
+        try {
+            $this->banco = mysqli_connect($host, $user, $pswd, $db);
+            $this->banco->set_charset('utf8');
+        } catch (Exception $e) {
+            die("Erro na conexão com MySQL! " . $e->getMessage());
+        }
+    }
+
+    // método estático
+    static function getInstance()
+    {
+        // Já existe uma instância?
+        if (self::$instance == NULL)
+            self::$instance = new Conexao();   // Não existe, cria a instância 
+        return self::$instance;               // Já existe, simplesmente retorna
+    }
+
+    // Previne o uso de clone
+    private function __clone() {}
+
+    public function getBanco()
+    {
+        return $this->banco;
+    }
+}
