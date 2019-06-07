0ec1e5cb07a391cdf7e78563f21babb8ea744e9e
Hélio Júnior <heliojunior53@gmail.com>
2019-06-06 22:22:17 -0300

    Commit inicial

---
+++ menutopo.php
@@ -0,0 +1,38 @@
+<section class="menu cid-rrD02sHv3w" once="menu" id="menu1-1">
+
+    
+
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="index.html">
+                         <img src="assets/images/mbr-3-122x122.jpg" alt="Mobirise" title="" style="height: 3.8rem;">
+                    </a>
+                </span>
+                <span class="navbar-caption-wrap"><a href="home.php" class="navbar-caption text-black display-5" href="index.html">GEV</a></span>
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true"><li class="nav-item">
+                    <a href="index.php" class="nav-link link text-black display-7" href="page1.html"><span class="mbri-globe mbr-iconfont mbr-iconfont-btn"></span>
+                        &nbsp;Eventos</a>
+                </li>
+                <li class="nav-item"><a class="nav-link link text-black display-7" href="page2.html"><span class="mbri-info mbr-iconfont mbr-iconfont-btn"></span>
+                        Sobre</a></li><li class="nav-item">
+                    <a class="nav-link link text-black display-7" href="contato.html"><span class="mbri-mobile2 mbr-iconfont mbr-iconfont-btn"></span>
+                        Contatos&nbsp;</a>
+                </li><li class="nav-item"><a class="nav-link link text-black display-7" href="duvidas.php"><span class="mbri-question mbr-iconfont mbr-iconfont-btn"></span>
+                        Ajuda &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</a></li><li class="nav-item"><a class="nav-link link text-black display-7" href="https://mobirise.co"><span class="mbri-search mbr-iconfont mbr-iconfont-btn"><br></span>Pesquisar</a></li></ul>
+            
+        </div>
+    </nav>
+</section>
