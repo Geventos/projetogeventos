2a2749d733e3e092a223f1c681f2f61e6c0d48a6
cristovaolima <37450799+cristovaolima@users.noreply.github.com>
2019-06-14 09:54:11 -0300

    Commit inicial

---
+++ rodapesite.php
@@ -0,0 +1,10 @@
+<section once="footers" class="cid-rsFRiw4o2Z" id="footer6-1j">
+  <div class="container">
+      <div class="media-container-row align-center mbr-white">
+          <div class="col-12">
+              <p class="mbr-text mb-0 mbr-fonts-style display-7">
+                  © GEv 2019 - Gerenciador de Eventos</p>
+          </div>
+      </div>
+  </div>
+</section>
