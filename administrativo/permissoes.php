584b38115327924fceb1d9258b03ac36fe9485ea
cristovaolima <37450799+cristovaolima@users.noreply.github.com>
2019-06-16 11:49:25 -0300

    Correção de permissões

--- administrativo/permissoes.php
+++ administrativo/permissoes.php
@@ -1,3 +1,13 @@
+<?php
+ 	session_start();
+	if(!empty($_SESSION['id_usuario'])){
+	    require_once("classes/DAOEvento.php");	    
+	    $eventos = (new DAOEvento())->listar();
+	}else{
+		header("Location: ../login");
+	}
+	include_once("sessao.php");    
+?>
 <!DOCTYPE html>
 <head>
 	<title>SisGEv | Permissões</title>
@@ -25,52 +35,7 @@
 				<div class="progressbar-heading grids-heading">
 					<h2 align="center">Permissões de Usuário</h2>
 				</div>
-				<div class="panel panel-widget forms-panel">
-					<div class="form-title">
-						<div class="row">
-							<div class="col-sm-12">
-								<div class="col-md-10">
-									<p><a href="#"><i class=""></i></i></a> Conceder Permissões</p>
-								</div>
-								
-							</div>
-						</div>
-					</div>
-					<div class="bs-docs-example wow fadeInUp animated" data-wow-delay=".5s">
-						<table class="table table-hover">
-							<thead>
-								<tr>
-									<th>Tipo de permissão</th>
-									<th>Email do usuário</th>
-									<th></th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td align="center"><select>
-										<option>Selecione</option>
-										<option id="1">Organizador</option>
-										<option id="2">Sub-Organizador</option>
-									</select>
-								</td>
-								
-								<td align="center">
-									<input type="email" name="Email do Usuário" placeholder="Digite o email do usuário" >
-								</td>
-
-								<td align="center">
-								<form action="" >
-									<button accesskey= type="submit" class="btn btn-primary btn-block" title="Gerar Crachá do Evento."><i class=""></i> Salvar</button>
-								</form>
-								</td>
-									
-								</tr>
-							</tbody>
-							
-						</table>
-					</div>
-					
-				</div>
+				<?php require_once("permissao-root.php"); ?>
 			</div>
 		</div>
 	</div>
