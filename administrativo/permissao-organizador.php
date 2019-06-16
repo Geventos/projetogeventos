7b861aa8ef83c2bfc487452cf43c0f85d1a519aa
cristovaolima <37450799+cristovaolima@users.noreply.github.com>
2019-06-16 11:48:26 -0300

    Commit inicial

---
+++ administrativo/permissao-organizador.php
@@ -0,0 +1,38 @@
+<div class="panel panel-widget forms-panel">
+					<div class="form-title">
+						<div class="row">
+							<div class="col-sm-12">
+								<div class="col-md-10">
+									<p><a href="#"><i class=""></i></i></a> Conceder Permissões</p>
+								</div>								
+							</div>
+						</div>
+					</div>
+					<div class="bs-docs-example wow fadeInUp animated" data-wow-delay=".5s">
+						<table class="table table-hover">
+							<thead>
+								<tr>
+									<th>Tipo de permissão</th>
+									<th>Email do usuário</th>
+									<th></th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td align="center"><select>
+										<option id="1">Sub-Organizador</option>
+									</select>
+								</td>								
+								<td align="center">
+									<input type="email" name="Email do Usuário" placeholder="Digite o email do usuário" >
+								</td>
+								<td align="center">
+								<form action="" >
+									<button accesskey= type="submit" class="btn btn-primary btn-block" title="Gerar Crachá do Evento."><i class=""></i> Salvar</button>
+								</form>
+								</td>									
+								</tr>
+							</tbody>							
+						</table>
+					</div>					
+				</div>
