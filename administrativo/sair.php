ca3e7abf673e984e6b362012f4aed6b6e3c76c2f
cristovaolima <37450799+cristovaolima@users.noreply.github.com>
2019-06-16 11:48:05 -0300

    Update sair.php

--- administrativo/sair.php
+++ administrativo/sair.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-    unset($_SESSION['id_usuario'], $_SESSION['nome_completo'], $_SESSION['email'], $_SESSION['permissao']);
+    unset($_SESSION['id_usuario'], $_SESSION['nome'], $_SESSION['email'], $_SESSION['permissao']);
     $_SESSION['msg'] = "Deslogado!";
-    header("Location: login.php");
+    header("Location: ../login");
 ?>
